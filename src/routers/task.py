91bd7cfedadf4ff5d0691f856fcc0fc53ec06b9e
zozi <zfernandez@arkusnexus.com>
2023-10-31 09:57:04 -0600

    Added typing

--- src/routers/task.py
+++ src/routers/task.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 from fastapi import APIRouter, HTTPException
 from prisma import errors
+from prisma.models import Task as TaskRecord
 
 from controllers import Task
 from schemas.task import TaskModel, ResponseTaskModel
@@ -10,19 +11,19 @@ router = APIRouter(prefix="/tasks")
 
 
 @router.get("/all", response_model=list[TaskModel])
-async def all_tasks():
+async def all_tasks() -> list[TaskRecord]:
     objects = await Task.find_many()
     return objects
 
 
 @router.post("/create", response_model=ResponseTaskModel)
-async def create_task(data: TaskModel):
+async def create_task(data: TaskModel) -> TaskRecord:
     obj = await Task.create(data=data.model_dump())
     return obj
 
 
 @router.get("/{pk}")
-async def get_task_by_id(pk: int):
+async def get_task_by_id(pk: int) -> TaskRecord:
     try:
         obj = await Task.find_first_or_raise(where={"id": pk})
         return obj
@@ -31,7 +32,7 @@ async def get_task_by_id(pk: int):
 
 
 @router.put("/edit/{pk}", response_model=ResponseTaskModel)
-async def edit_task(pk: int, data: TaskModel):
+async def edit_task(pk: int, data: TaskModel) -> TaskRecord:
     obj = await Task.update(where={"id": pk}, data=data.model_dump())
     if not obj:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Item not found")
