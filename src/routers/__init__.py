2c87293389a5008ab1f305e76c158d6513e591cb
zozi <zfernandez@arkusnexus.com>
2023-11-01 10:20:10 -0600

    Rename file of user

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from routers import home, task, users
+from routers import home, task, user
 
 
 api = APIRouter(prefix="/api")
 api.include_router(home.router)
 api.include_router(task.router)
-api.include_router(users.router)
+api.include_router(user.router)
